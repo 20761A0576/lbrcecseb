a2ffefeaedf9b3e9d644638c3ba03c891ec61a0d
AdmiN <AdmiN@DESKTOP-M101FJ5>
2023-09-08 15:00:27 +0530

    Manohar

--- src/com/lbrce/CSEBProject/TeamLead1.java
+++ src/com/lbrce/CSEBProject/TeamLead1.java
@@ -5,11 +5,17 @@ public class TeamLead1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		fun();
+		run();
 	}
 	
 	public static void fun() {
 		// TODO Auto-generated method stub
 		System.out.println("Fun Method");
+
+	}
+	public static void run() {
+		// TODO Auto-generated method stub
+		System.out.println("run Method");
 
 	}
 
