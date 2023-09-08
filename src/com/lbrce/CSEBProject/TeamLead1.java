9fb8e9ad2db6db81e09827f4caeadd50cb96e443
Admin <Admin@DESKTOP-2S7HFKC>
2023-09-08 14:15:40 +0530

    First Message

---
+++ src/com/lbrce/CSEBProject/TeamLead1.java
@@ -0,0 +1,16 @@
+package com.lbrce.CSEBProject;
+
+public class TeamLead1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		fun();
+	}
+	
+	public static void fun() {
+		// TODO Auto-generated method stub
+		System.out.println("Fun Method");
+
+	}
+
+}
